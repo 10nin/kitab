41691f63f823dff623975d8f79d1c146c26b0b78
Akira Ota 10nin <akira19870221@gmail.com>
2018-06-07 20:33:33 +0900

    Add http client library and first sample call of function.

--- src/kitab/core.clj
+++ src/kitab/core.clj
@@ -1,6 +1,11 @@
 (ns kitab.core)
+(require '[clj-http.client :as client])
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defrecord Book [book-name, barcode])
+
+(defn gen-book [name, bcd]
+  (->Book name, bcd))
+
+;; バーコードから楽天booksで書籍を探す
+;; https://books.rakuten.co.jp/search/nm?sitem=9784274067211 的な。
+(client/get "https://books.rakuten.co.jp/search/nm?sitem=9784274067211")
